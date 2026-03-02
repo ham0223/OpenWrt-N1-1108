f2e8a6721f4116eb740c9c82e1fb572a92727566
ham0223 <126130711+ham0223@users.noreply.github.com>
2026-03-02 11:15:03 +0800

    Update shadowsocks-libev build flags to disable patchelf
    Force disable patchelf for shadowsocks-libev to avoid static ELF errors by modifying the Makefile.

--- N1/Immortalwrt/diy-part2.sh
+++ N1/Immortalwrt/diy-part2.sh
@@ -35,9 +35,19 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 sed -i 's/<%:Up%>/<%:Move up%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
 sed -i 's/<%:Down%>/<%:Move down%>/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/tblsection.htm
 
+
+
+
+
 # 3月1日尝试解决shadowsocks-libev编译失败的问题
-# Fix shadowsocks-libev patchelf error
-sed -i '/PKG_USE_MIPS16/a\PKG_BUILD_FLAGS:=no-patchelf' package/feeds/small/shadowsocks-libev/Makefile
+# 强制禁用 shadowsocks-libev 的 patchelf，避免静态 ELF 报错
+makefile="package/feeds/small/shadowsocks-libev/Makefile"
+# 删除已有的 PKG_BUILD_FLAGS（避免重复或冲突）
+sed -i '/PKG_BUILD_FLAGS/d' "$makefile"
+# 追加 no-patchelf（确保最终生效）
+echo 'PKG_BUILD_FLAGS:=no-patchelf' >> "$makefile"
+
+
 
 
 
